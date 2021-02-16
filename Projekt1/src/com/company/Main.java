b958b3584934bfda9cf220c3873b48af91f8b545
lukasklvac <62391870+lukasklvac@users.noreply.github.com>
2021-02-16 11:59:45 +0100

    Add files via upload

---
+++ Projekt1/src/com/company/Main.java
@@ -0,0 +1,41 @@
+package com.company;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class Main {
+
+    public static void main(String[] args) {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            String url = "jdbc:mysql://localhost:3306/autoservis?useUnicode=true&useJDBCCCompilantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC";
+            String user = "root";
+            String pass = "";
+            Connection con = DriverManager.getConnection(url, user, pass);
+            if(con!=null)
+            {
+                System.out.println("Jede");
+
+            }
+
+            String query = "select * from vozy";
+            var statement = con.prepareStatement(query);
+            ResultSet r = statement.executeQuery();
+            while (r.next())
+            {
+                int id = r.getInt("id");
+                String brand = r.getString("Znacka");
+                        String model = r.getString("Model");
+                int year = r.getInt("Rok vyroby");
+                System.out.printf("%-10s%-20s%-20s%-10s\n",id, brand,model, year);
+            }
+
+        } catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+}
